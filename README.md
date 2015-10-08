5b09e82055341ec4b9ac24d31e3362be5b89cb09
mattdesl <dave.des@gmail.com>
2015-10-08 12:09:14 -0400

    bam!

---
+++ README.md
@@ -0,0 +1,47 @@
+# get-ports
+
+[![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+
+Finds multiple open ports after your specified base ports, and below the max range. 
+
+Unlike [getport](https://github.com/mikeal/getport) or [get-port](https://github.com/sindresorhus/get-port), this is useful for situations where you need multiple servers to run on open ports.
+
+If not all ports could be found, the error callback is triggered.
+
+## Install
+
+```sh
+npm install get-ports --save
+```
+
+## Example
+
+The resulting `ports` array is parallel to your input (base) ports.
+
+```js
+var getPorts = require('get-ports')
+
+getPorts([ 9966, 3000 ], function (err, ports) {
+  if (err) throw new Error('could not open servers')
+  
+  var [ a, b ] = ports
+  console.log('server 1', a)
+  console.log('server 2', b)
+})
+```
+
+## Usage
+
+[![NPM](https://nodei.co/npm/get-ports.png)](https://www.npmjs.com/package/get-ports)
+
+#### `getPorts(basePorts, [maxPort], callback)`
+
+For the given array of `basePorts`, tries to find the next available port from each one. This keeps track of available ports to ensure there are no conflicts in the final result.
+
+If `maxPort` is specified, the portfinding will fail when it reaches that maximum port. Defaults to 60000.
+
+The callback is called with `(err, ports)`, where `err` will be an Error if any of the portfindings failed (i.e. no open ports within range). If successful, `err` will be null and `ports` will be an array, parallel to `basePorts`, with the found port numbers.
+
+## License
+
+MIT, see [LICENSE.md](http://github.com/Jam3/get-ports/blob/master/LICENSE.md) for details.
---
+++ test.js
@@ -0,0 +1,67 @@
+var getPorts = require('./')
+var test = require('tape')
+var net = require('net')
+
+test('gets multiple open ports', function (t) {
+  t.plan(1)
+  var server = net.createServer()
+  server.listen(8000, function () {
+    getPorts([ 8000, 8000 ], function (err, ports) {
+      server.close()
+      if (err) return t.fail(err)
+      t.deepEqual(ports, [ 8001, 8002 ], 'gets next available ports')
+    })
+  })
+})
+
+test('gets multiple open ports', function (t) {
+  t.plan(1)
+  var server = net.createServer()
+  server.listen(8000, function () {
+    var server2 = net.createServer()
+    server2.listen(8001, function () {
+      getPorts([ 8001, 8000 ], function (err, ports) {
+        server.close()
+        server2.close()
+        if (err) return t.fail(err)
+        t.deepEqual(ports, [ 8002, 8003 ], 'gets next available ports')
+      })
+    })
+  })
+})
+
+test('gets multiple open ports', function (t) {
+  t.plan(1)
+  var server = net.createServer()
+  server.listen(59998, function () {
+    getPorts([ 59998 ], function (err, ports) {
+      server.close()
+      if (err) return t.fail(err)
+      t.deepEqual(ports, [ 59999 ], 'gets next available ports')
+    })
+  })
+})
+
+test('accepts range', function (t) {
+  t.plan(1)
+  var server = net.createServer()
+  server.listen(8005, function () {
+    getPorts([ 8005, 8005 ], 8007, function (err, ports) {
+      server.close()
+      if (!err) return t.fail(new Error('expected error, not ports ' + ports))
+      t.ok(err instanceof Error, 'no open ports within range')
+    })
+  })
+})
+
+test('returns error when out of ports', function (t) {
+  t.plan(1)
+  var server = net.createServer()
+  server.listen(59998, function () {
+    getPorts([ 59998, 59998 ], function (err, ports) {
+      server.close()
+      if (!err) return t.fail(new Error('expected error, not ' + ports))
+      t.ok(err instanceof Error, 'out of ports')
+    })
+  })
+})
